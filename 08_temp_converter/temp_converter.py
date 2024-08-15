503574054371e65faeeac4617f57e0c3c923ef12
DavidSokoya <sokoya08@gmail.com>
2024-08-15 23:37:43 +0100

    calc

--- 08_temp_converter/temp_converter.py
+++ 08_temp_converter/temp_converter.py
@@ -1,5 +1,9 @@
-celcius = float(input("Enter temperature in degree celcius: "))
+temp = float(input("Enter temperature in (F/C): "))
 
-faregnheit = celcius * 9 / 32
+
+if temp == 'C':
+  temp = temp * 9 / 32
+  print(f"{temp} degrees celcius to f is: {temp}f")
+elif temp == 'F':
 
 print(f"{celcius} degrees celcius to f is: {faregnheit}f")
