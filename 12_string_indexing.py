26fa737c1d01f4c28edd9ca0f3107523826282cd
DavidSokoya <sokoya08@gmail.com>
2024-08-17 20:18:28 +0100

    indexing

--- 12_string_indexing.py
+++ 12_string_indexing.py
@@ -1,1 +1,9 @@
 # strings are sequences of characters that can be indexed, meaning you can access individual characters or substrings using their position in the string. using [] 
+# [start : stop : step]
+
+credit_number = '1234-5678-9012-2345'
+
+# print(credit_number[0:4])
+print(credit_number[5:9])
+print(credit_number[-4])
+print(credit_number[::3])
