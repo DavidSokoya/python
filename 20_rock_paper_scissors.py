ee704fc7ff58303a74833c9433286e883a1be16b
DavidSokoya <sokoya08@gmail.com>
2024-08-21 23:55:22 +0100

    Refractoring

--- 20_rock_paper_scissors.py
+++ 20_rock_paper_scissors.py
@@ -3,16 +3,16 @@ import random
 choices = ['rock', 'paper', 'scissors']
 def get_valid_input():
    while True:
-      player = input('Enter rock paperor scissors, or q to quit: ').lower()
-      while True:
-        if  player == 'q':
-          break;
-        if player  in choices:
-          return player
-        else:
-          print('Invalid input.. Please enter rock paper or scissors')
+      player = input("Enter rock paperor scissors, or 'q' to quit: ").lower()
+      if player  in choices or player == 'q':
+         return player
+      else:
+         print('Invalid input.. Please enter rock paper or scissors')
+
 while True:
     player = get_valid_input()
+    if  player == 'q':
+        break
     computer = random.choice(choices)
     if player == computer:
       print('Draw!')
