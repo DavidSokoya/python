f30ea92e5b9fbafe61db4c81af16d0f59f8b746c
DavidSokoya <sokoya08@gmail.com>
2024-09-08 20:09:34 +0100

    string template

---
+++ codewars/string_template.py
@@ -0,0 +1,4 @@
+# Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+def build_string(*template):
+    return f"I like {', '.join(template)}!"
