f1274a2c62c061b99e88d903982defecb8d84f1e
DavidSokoya <sokoya08@gmail.com>
2024-09-09 23:31:37 +0100

    name on billboard

--- codewars/name_on_billboard.py
+++ codewars/name_on_billboard.py
@@ -1,4 +1,4 @@
-# You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
+# You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for java a).
 
 # You can not use multiplier "*" operator.
 
