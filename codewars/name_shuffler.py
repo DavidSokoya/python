46798c691775b41059959e9df65be490f6e2219f
DavidSokoya <sokoya08@gmail.com>
2024-09-11 08:51:07 +0100

    Reversed Word

--- codewars/name_shuffler.py
+++ codewars/name_shuffler.py
@@ -10,3 +10,10 @@ def name_shuffler(str_):
     last_name = new_str[1]
     
     return f"{last_name} {first_name}"
+
+def name_shuffler(str_):
+    [first, last] = str_.split()
+    return last + " " + first
+
+def name_shuffler(str_):
+    return ' '.join(str_.split(' ')[::-1])
